2e214769517175af9033077df84399e9ad15843a
sbadiane <sbadiane@unikerp.com>
2025-05-30 12:57:14 +0000

    type de produit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Syll burger',
+    'description': 'My module description',
+    'depends': ['base','sale'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ models/produit.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+class CustomProduct(models.Model):
+    _inherit = 'product.template'
+
+    product_type = fields.Selection([
+        ('berger', 'BURGERS'),
+        ('smashs', 'SMASHS'),
+        ('chicken_burger', 'CHICKEN BURGERS'),
+        ('menu_enfant', 'MENU ENFANTS'),
+        ('snacks', 'SNACKS'),
+        ('sauce', 'SAUCE'),
+        ('menu_enfant', 'MENU ENFANTS'),
+        ('milk_shakes', 'MILK-SHAKES'),
+        ('dessert', 'DESSERTS'),
+        ('boissons_eau', 'BOISSONS ET EAU'),
+    ], string='Type du produit')
